c1251551ced9dee0d9818059dfe32c7bd90200ef
blubincod <bluebinarycode@gmail.com>
2024-08-08 19:43:43 +0900

    Initial commit

---
+++ src/main/java/com/concord/petmily/PetmilyApplication.java
@@ -0,0 +1,13 @@
+package com.concord.petmily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetmilyApplication.class, args);
+    }
+
+}
