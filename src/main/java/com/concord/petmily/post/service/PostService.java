7d9f8532df2b6dee8ed830878e880b97840ef8a3
blubincod <bluebinarycode@gmail.com>
2024-08-09 20:02:24 +0900

    가이드 코드 작성

---
+++ src/main/java/com/concord/petmily/post/service/PostService.java
@@ -0,0 +1,14 @@
+package com.concord.petmily.post.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 게시물 서비스
+ * 비즈니스 로직을 처리
+ */
+@Service
+public class PostService {
+    public void createPost() {
+
+    }
+}
