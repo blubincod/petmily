43f72e639d109b3b0eda234e2231bdccbe0f839d
JYP <bluebinarycode@gmail.com>
2024-08-12 16:19:19 +0900

    feat: 산책 관련 기능의 엔드포인트 정의

--- src/main/java/com/concord/petmily/walk/controller/WalkController.java
+++ src/main/java/com/concord/petmily/walk/controller/WalkController.java
@@ -2,7 +2,6 @@ package com.concord.petmily.walk.controller;
 
 import com.concord.petmily.walk.dto.WalkLocationDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -11,18 +10,18 @@ import java.util.Map;
 
 /**
  * 산책 관련 컨트롤러
- * <p>
- * - 산책 위도, 경도 가져오기
+ * - 산책 위치 기록
  * - 산책 전체 정보 조회
  * - 산책 상세 정보 조회
- * - 산책 목표 설정 (관리자)
+ * - 산책 목표 설정 - 관리자
  * - 산책 목표 조회
+ * - 산책 목표 선택
  */
 @RestController
 @RequestMapping("/api/v1/walks")
 public class WalkController {
     /**
-     * 산책 위치(위도, 경도) 저장
+     * 산책 위치(위도, 경도, 측정된 시간) 저장
      *
      * @param
      * @return 생성된 게시물의 정보와 HTTP 200 상태를 반환
@@ -38,6 +37,59 @@ public class WalkController {
         return ResponseEntity.ok(response);
     }
 
-//    @PostMapping
-//    public ResponseEntity<?>
+    /**
+     * 산책 목록 조회
+     *
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<?> getAllWalks() {
+        // TODO 산책 목록 조회 로직
+        return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 산책 정보 조회
+     *
+     * @return
+     */
+    @GetMapping("/{walkId}")
+    public ResponseEntity<?> getWalkDetail() {
+        // TODO 산책 정보 조회 로직
+        return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 산책 목표 생성 - 관리자
+     *
+     * @return
+     */
+    @PostMapping("goals")
+    public ResponseEntity<?> createWalkGoal() {
+        // TODO 관리자 권한 확인 로직 추가
+        // TODO 산책 목표 생성 로직
+        return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 산책 목표 목록 조회
+     * @return
+     */
+    @GetMapping("/goals")
+    public ResponseEntity<?> getWalkGoals() {
+        // TODO 사용 산책 목표 목록을 조회하는 로직
+        return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 산책 목표 선택
+     * @param userId
+     * @param goalId
+     * @return
+     */
+    @PostMapping("/goals/{goadId}")
+    public ResponseEntity<Void> selectWalkGoal(@RequestParam Long userId, @RequestParam Long goalId) {
+        // TODO 사용자가 산책 목표를 선택하는 로직
+        return ResponseEntity.ok(null);
+    }
 }
--- src/main/java/com/concord/petmily/walk/repository/WalkRepository.java
+++ src/main/java/com/concord/petmily/walk/repository/WalkRepository.java
@@ -4,7 +4,8 @@ import com.concord.petmily.walk.entity.Walk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- *
+ * 산책 저장소 인터페이스
+ * 데이터베이스와의 CRUD 작업을 처리
  */
 public interface WalkRepository extends JpaRepository<Walk, Long> {
 
