e552aaa33cae3cc6924ccae3b4793cd7d70190a4
minzion <aert12435@gmail.com>
2024-08-14 23:51:26 +0900

    로그인 로직 비작동 이슈 해결

--- src/main/java/com/concord/petmily/auth/dto/JwtProperties.java
+++ src/main/java/com/concord/petmily/auth/dto/JwtProperties.java
@@ -2,15 +2,15 @@ package com.concord.petmily.auth.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
 @Component
-@ConfigurationProperties("jwt")
 public class JwtProperties {
 
     private String issuer;
+    @Value("${spring.jwt.secret-key}")
     private String secretKey;
 }
