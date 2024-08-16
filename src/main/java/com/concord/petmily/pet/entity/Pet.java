50444b78478e4df36bcf15ebb8fb0695ac0fd706
minzion <aert12435@gmail.com>
2024-08-17 02:39:07 +0900

    feat - 팻 crud 구현

--- src/main/java/com/concord/petmily/pet/entity/Pet.java
+++ src/main/java/com/concord/petmily/pet/entity/Pet.java
@@ -1,85 +1,76 @@
 package com.concord.petmily.pet.entity;
 
 import com.concord.petmily.pet.dto.PetDto;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Table(name = "pet")
+@Getter
+@Setter
 @RequiredArgsConstructor
 public class Pet {
 
-  // 반려동물의 기본 키
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  // 사용자의 ID
   @Column(nullable = false)
   private Long userId;
 
-  // PetDto의 Category 타입
   @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private Category category;
 
-  // 반려동물의 품종
-  @Column(nullable = false)
+  @Column(nullable = false, length = 50)
   private String brand;
 
-  // 반려동물의 생년월일
   @Column(nullable = false)
   private LocalDate birthDate;
 
-  // 반려동물의 나이
   @Column(nullable = false)
   private int age;
 
-  // 반려동물의 이름
-  @Column(nullable = false)
+  @Column(nullable = false, length = 30)
   private String name;
 
-  // 반려동물의 Gender 타입
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private Gender gender;
 
-  // 반려동물의 중성화 여부
   @Column(nullable = false)
-  private boolean isPetsNeuter;
+  private Boolean isPetsNeuter;
 
-  // 반려동물의 무게
   @Column(nullable = false)
   private double weight;
 
-  // 반려동물의 Status 타입
   @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private Status status;
 
-  // 반려동물의 이미지
+  @Column(length = 255)
   private String image;
 
-  // 반려동물의 칩 번호
+  @Column(unique = true, length = 20)
   private String chip;
 
-  // 생성 시점
   @CreatedDate
+  @Column(nullable = false, updatable = false)
   private LocalDateTime createAt;
 
-  // 수정 시점
   @LastModifiedDate
+  @Column(nullable = false)
   private LocalDateTime modifiedAt;
 
-
   private Pet(Long userId, Category category, String brand, LocalDate birthDate, int age,
-      String name, Gender gender, boolean isPetsNeuter, double weight, Status status,
-      String image, String chip) {
+      String name, Gender gender, Boolean isPetsNeuter, double weight, Status status,
+       String chip) {
     this.userId = userId;
     this.category = category;
     this.brand = brand;
@@ -90,18 +81,13 @@ public class Pet {
     this.isPetsNeuter = isPetsNeuter;
     this.weight = weight;
     this.status = status;
-    this.image = image;
     this.chip = chip;
   }
 
   public static Pet from(Long userId, PetDto.Create request) {
-   return new Pet(userId, request.getPetsCategory(), request.getPetsBreed(), request.getBirthDate(),
-        request.getPetsAge(),
-        request.getPetsName(), request.getPetsGender(), request.isPetsNeuter(),
+    return new Pet(userId, request.getPetsCategory(), request.getPetsBreed(), request.getBirthDate(),
+        request.getPetsAge(), request.getPetsName(), request.getPetsGender(), request.isPetsNeuter(),
         request.getPetsWeight(), Status.ACTIVE,
-        request.getPetsImage().isEmpty() ? null : request.getPetsImage(),
         request.getPetsChip().isEmpty() ? null : request.getPetsChip());
   }
-
-
 }
--- src/main/java/com/concord/petmily/pet/repository/PetRepository.java
+++ src/main/java/com/concord/petmily/pet/repository/PetRepository.java
@@ -1,6 +1,10 @@
 package com.concord.petmily.pet.repository;
 
 import com.concord.petmily.pet.entity.Pet;
+import com.concord.petmily.pet.entity.Status;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 /**
@@ -10,4 +14,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PetRepository extends JpaRepository<Pet,Long> {
 
+  Page<Pet>findByUserIdAndStatus(Long userId, Status status,Pageable pageable);
+  Optional<Pet> findByIdAndUserId(Long id,Long userId);
 }
