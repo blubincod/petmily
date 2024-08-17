3205df36158ee623d275f2c897da93b66bb7a1c4
JYP <bluebinarycode@gmail.com>
2024-08-17 10:41:24 +0900

    fix: CSRF 비활성화하여 POST 요청의 403 오류 해결, closes #16

--- src/main/java/com/concord/petmily/auth/security/SecurityConfig.java
+++ src/main/java/com/concord/petmily/auth/security/SecurityConfig.java
@@ -9,58 +9,74 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+/**
+ * Spring Security 보안 설정 클래스
+ */
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class SecurityConfig {
 
     private final UserDetailService userService;
 
+    /**
+     * WebSecurity에 대한 커스터마이징
+     * 특정 요청을 Spring Security 필터 체인이 무시하도록 설정
+     */
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
+                .requestMatchers(toH2Console()) // H2 데이터베이스 콘솔에 대한 요청 패턴을 무시
+                .requestMatchers("/static/**"); // '/static/' 경로 아래의 모든 리소스에 대한 요청을 무시
     }
 
+    /**
+     * Spring Security에서 요청에 대한 인증 및 권한 부여를 처리하기 위해 사용되는 필터 모음
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable) // Cross-Site Request Forgery 보호를 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션을 생성하지 않는 상태 없는(stateless) 정책을 설정
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                    .requestMatchers("/**").permitAll()//원활한 개발활경을위한 모든경로 접근 허용
-                        .requestMatchers("/login", "/users/signup", "/api/v1/users/signup").permitAll() // 인증 없이 접근 허용
-                        .requestMatchers("/api/v1/**").permitAll() // 조회 요청을 인증 없이 접근 허용
+                        .requestMatchers("/**").permitAll()// FIXME 원활한 개발활경을위한 모든경로 접근 허용
+                        .requestMatchers("/", "/login", "/users/signup", "/api/v1/users/signup").permitAll() // 인증 없이 접근 허용
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
                 .formLogin(customizer -> customizer
                         .loginPage("/login")
-                        .defaultSuccessUrl("/home", true)
+                        .defaultSuccessUrl("/", true)
                 )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
                 )
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/v1/users/signup", "/api/v1/users/login", "/api/v1/users/logout") // CSRF 비활성화
-                )
                 .build();
     }
 
+    /**
+     * 사용자 인증을 처리하는 매니저 설정
+     */
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder
                 .userDetailsService(userService)
-                .passwordEncoder(bCryptPasswordEncoder);
+                .passwordEncoder(bCryptPasswordEncoder); // 해당 비밀번호 인코더로 비밀번호 해싱
         return authenticationManagerBuilder.build();
     }
 
+    /**
+     * BCrypt 알고리즘을 사용하여 비밀번호를 해싱
+     */
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
 insert into postcategory(category_id, category_name) values (1, '자유게시판');
 insert into postcategory(category_id, category_name) values (2, '질의응답게시판');
 insert into postcategory(category_id, category_name) values (3, '리뷰게시판');
-
-insert into post(post_id, user_id, category_id, thumbnail_path, title, content, content_image_path, view_count, like_count, status, created_at, updated_at, attachment_paths) values (1, 1, 2, 'abc', 'abc', 'abc', 'abc', 0, 0, 0, now(), now(), 'abc');
