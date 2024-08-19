0d73796441dde33661e50ced4358ceb2af0ebe63
minzion <aert12435@gmail.com>
2024-08-19 20:09:40 +0900

    질병 CRUD 구현

--- src/main/java/com/concord/petmily/disease/entity/Disease.java
+++ src/main/java/com/concord/petmily/disease/entity/Disease.java
@@ -1,15 +1,25 @@
 package com.concord.petmily.disease.entity;
 
+import com.concord.petmily.disease.dto.DiseaseDto;
+import com.concord.petmily.pet.entity.Category;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "disease")
 @RequiredArgsConstructor
+@Data
 public class Disease {
 
   //기본 키
@@ -22,4 +32,30 @@ public class Disease {
 
   // 질병의 설명
   private String description;
+  // 접종 기한
+  private Integer vaccinationDeadline;
+  @ElementCollection
+  @Enumerated(EnumType.STRING)
+  private Set<Category> animalCategories;
+
+  public Disease(String name, String description, Integer vaccinationDeadline, Set<Category> animalCategories) {
+    this.name = name;
+    this.description = description;
+    this.vaccinationDeadline = vaccinationDeadline;
+    this.animalCategories = animalCategories;
+  }
+
+  public static Disease from(DiseaseDto.Create request) {
+    Set<Category> categories = request.getAnimalCategories().stream().map(category -> {
+          try {
+            return Category.valueOf(category.toString().toUpperCase());
+          } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Invalid category name: " + category);
+          }
+        })
+        .collect(Collectors.toSet());
+    return new Disease(request.getName(), request.getDescription(), request.getVaccinationDeadline(), categories);
+  }
+
+
 }
