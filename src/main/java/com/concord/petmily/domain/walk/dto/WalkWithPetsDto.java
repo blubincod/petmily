c2e8bd66683eb434c462c7cc2f35183993ab8a78
JYP <bluebinarycode@gmail.com>
2024-08-24 03:04:34 +0900

    feat: 산책 기록 조회 시 반려동물 아이디 리스트 포함

--- src/main/java/com/concord/petmily/domain/walk/dto/WalkWithPetsDto.java
+++ src/main/java/com/concord/petmily/domain/walk/dto/WalkWithPetsDto.java
@@ -1,25 +1,36 @@
 package com.concord.petmily.domain.walk.dto;
 
 import com.concord.petmily.domain.walk.entity.Walk;
+import com.concord.petmily.domain.walk.entity.WalkStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@SuperBuilder
-public class WalkWithPetsDto extends WalkDto {
+@Builder
+public class WalkWithPetsDto {
     private List<Long> petIds;
+    private Long walkId;
+    private double distance;
+    private double duration;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private WalkStatus status;
 
+    // WalkWithPetsDto 객체를 생성
     public static WalkWithPetsDto fromEntity(Walk walk, List<Long> petIds) {
         return WalkWithPetsDto.builder()
+                .petIds(petIds)
                 .walkId(walk.getId())
                 .distance(walk.getDistance())
                 .duration(walk.getDuration())
                 .startTime(walk.getStartTime())
                 .endTime(walk.getEndTime())
                 .status(walk.getWalkStatus())
-                .petIds(petIds)
                 .build();
     }
 }
