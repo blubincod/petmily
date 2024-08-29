780565373d8dfba43f9b3c6269e746b031fdd887
totorotail <jminkyun@gmail.com>
2024-08-29 21:47:35 +0900

    feat: 회원 정보 수정 추가

--- src/main/java/com/concord/petmily/domain/user/service/UserService.java
+++ src/main/java/com/concord/petmily/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.concord.petmily.domain.user.service;
 
+import com.concord.petmily.domain.user.dto.AddAdminRequest;
 import com.concord.petmily.domain.user.dto.AddUserRequest;
 import com.concord.petmily.domain.user.entity.User;
 
@@ -10,12 +11,12 @@ public interface UserService {
     User findById(Long userId);
     // 회원가입
     Long registerUser(AddUserRequest dto);
+    // 회원 정보 수정
+    User updateUser(String username, Long userId, AddUserRequest dto);
+    // 회원 Role, Status 변경 (관리자 전용)
+    User updateUserByAdmin(String username, Long userId, AddAdminRequest dto);
     // 회원 삭제 기능
     void deleteUser(String username, Long userId);
-    // 회원 정지 기능 (관리자 전용)
-    void suspendUser(String username, Long userId);
-    // 회원 정지 해제 기능 (관리자 전용)
-    void unsuspendUser(String username, Long userId);
     // 회원 통계 조회 (관리자 전용)
     String getStatistics(String username);
 }
